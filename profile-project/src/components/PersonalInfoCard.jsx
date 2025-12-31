20191cfdeecd286c5931a135ef588693575a3196
ZabeenaSherzoie <zabeenasherzoie@gmail.com>
2025-12-31 17:38:52 +0430

    Added PersonalInfoCard.jsx and Row.jsx

---
+++ profile-project/src/components/PersonalInfoCard.jsx
@@ -0,0 +1,15 @@
+import Row from "./Row"
+export default function PersonalInfoCard(info){
+    return(
+        <div className="card infoCard">
+            <h2 className= "cardTitle">Personal Information</h2>
+            <div className="rows">
+                {
+                    object.keys(info).map((label) =>{
+                        <Row  label={label} value={info[label]}></Row>
+                    })
+                }
+            </div>
+        </div>
+    )
+}
---
+++ profile-project/src/components/Row.jsx
@@ -0,0 +1,8 @@
+export default function Row({label, value}){
+    return (
+        <div className="row">
+            <span className="rowLabel"></span>
+            <span className="rowValue"></span>
+        </div>
+    )
+}
